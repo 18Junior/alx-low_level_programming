daa592736feb58a44332afa86f9a6daf0c684d23
18Junior <ntlakusojunior7@gmail.com>
2023-07-16 13:50:27 -0700

    4 print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,12 @@ int main(void)
 	char c;
 
 	c = 'a';
-	while 
+	while
 		(c <= 'z') {
 			if ((c != 'q' && c != 'e') && c <= 'z')
 				putchar(c);
 			c++;
 		}
 	putchar('\n');
-
 	return (0);
 }
